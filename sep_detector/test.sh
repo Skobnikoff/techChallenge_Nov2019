5be489fcb995cd2fef98794dfb140f4c58f18f7f
Arthur Skobnikov <arthur.skobnikov@gmail.com>
2019-11-04 23:11:12 +0100

    (ex.1|sep_detect): add test CSV files, test script

---
+++ sep_detector/test.sh
@@ -0,0 +1,20 @@
+echo "data/9Lines4ColsCommaUkr.csv"
+sep_detector data/9Lines4ColsCommaUkr.csv
+
+echo "data/1000Lines100ColsTabRandNoiseQuoted.txt"
+sep_detector data/1000Lines100ColsTabRandNoiseQuoted.txt
+
+echo "data/1000Lines14ColsBarEng.csv"
+sep_detector data/1000Lines14ColsBarEng.csv    
+
+echo "data/1000Lines14ColsSemicolonEng.csv"
+sep_detector data/1000Lines14ColsSemicolonEng.csv
+
+echo "data/1000Lines100ColsTabRandNoise.txt"
+sep_detector data/1000Lines100ColsTabRandNoise.txt
+
+echo "data/1000Lines14ColsCommaEng.csv"
+sep_detector data/1000Lines14ColsCommaEng.csv
+
+echo "data/36635Lines18ColsSlashREOLCommaEng.csv"
+sep_detector data/36635Lines18ColsSlashREOLCommaEng.csv
