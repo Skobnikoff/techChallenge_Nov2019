5056596d6761595c44aedfdc108225d29845d147
Arthur Skobnikov <arthur.skobnikov@gmail.com>
2019-11-08 21:43:06 +0100

    (ex.3|master-slave|master): send and receive massages from slaves

--- master_slave/master.py
+++ master_slave/master.py
@@ -2,23 +2,37 @@ import time
 import zmq
 
 context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
 
+# socket communicating with clients
+server = context.socket(zmq.REP)
+server.bind("tcp://*:5555")
+
+# socket pushing work to slaves
+sender = context.socket(zmq.PUSH)
+sender.bind("tcp://*:5556")
+
+# socket receiving results from slaves
+receiver = context.socket(zmq.PULL)
+receiver.bind("tcp://*:5557")
+
+print("Waiting for a request from client...")
 while True:
     #  wait for client request
-    message = socket.recv()
+    message = server.recv()
     print("Received request: %s" % message)
 
     #  partition work
     # TODO
 
     # send tasks to slaves
-    # TODO
+    for i in range(5):
+        print("Send to slave task #{}".format(str(i)))
+        sender.send_string(u"Task from master #{}".format(str(i)))
 
     # wait for responses from slaves
-    # TODO
-
+    for i in range(5):
+        slave_response = receiver.recv_string()
+        print("Response from slave: {}".format(slave_response))
     # assemble results
     # TODO
 
@@ -26,5 +40,5 @@ while True:
 
     #  send reply back to client
     response = b"World"
-    print("Send response: %s" % response)
-    socket.send(response)
+    print("Send response to client: %s" % response)
+    server.send(response)
