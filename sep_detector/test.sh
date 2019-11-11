a758b1e24621adf7ad2c266f907fc1624879e7e4
Arthur Skobnikov <arthur.skobnikov@gmail.com>
2019-11-11 17:24:13 +0100

    (ex.1|sep_detect): move data folder in root dir

--- sep_detector/test.sh
+++ sep_detector/test.sh
@@ -1,23 +1,23 @@
-echo "data/9Lines4ColsCommaUkr.csv"
-sep_detector data/9Lines4ColsCommaUkr.csv
+echo "../data/9Lines4ColsCommaUkr.csv"
+sep_detector ../data/9Lines4ColsCommaUkr.csv
 
-echo "data/1000Lines100ColsTabRandNoiseQuoted.txt"
-sep_detector data/1000Lines100ColsTabRandNoiseQuoted.txt
+echo "../data/1000Lines100ColsTabRandNoiseQuoted.txt"
+sep_detector ../data/1000Lines100ColsTabRandNoiseQuoted.txt
 
-echo "data/1000Lines14ColsBarEng.csv"
-sep_detector data/1000Lines14ColsBarEng.csv    
+echo "../data/1000Lines14ColsBarEng.csv"
+sep_detector ../data/1000Lines14ColsBarEng.csv
 
-echo "data/1000Lines14ColsSemicolonEng.csv"
-sep_detector data/1000Lines14ColsSemicolonEng.csv
+echo "../data/1000Lines14ColsSemicolonEng.csv"
+sep_detector ../data/1000Lines14ColsSemicolonEng.csv
 
-echo "data/1000Lines100ColsTabRandNoise.txt"
-sep_detector data/1000Lines100ColsTabRandNoise.txt
+echo "../data/1000Lines100ColsTabRandNoise.txt"
+sep_detector ../data/1000Lines100ColsTabRandNoise.txt
 
-echo "data/1000Lines14ColsCommaEng.csv"
-sep_detector data/1000Lines14ColsCommaEng.csv
+echo "../data/1000Lines14ColsCommaEng.csv"
+sep_detector ../data/1000Lines14ColsCommaEng.csv
 
-echo "data/36635Lines18ColsSlashREOLCommaEng.csv"
-sep_detector data/36635Lines18ColsSlashREOLCommaEng.csv
+echo "../data/36635Lines18ColsSlashREOLCommaEng.csv"
+sep_detector ../data/36635Lines18ColsSlashREOLCommaEng.csv
 
-echo "data/7Lines6ColsEmptyLinesCommaUkr.csv"
-sep_detector data/7Lines6ColsEmptyLinesCommaUkr.csv
+echo "../data/7Lines6ColsEmptyLinesCommaUkr.csv"
+sep_detector ../data/7Lines6ColsEmptyLinesCommaUkr.csv
