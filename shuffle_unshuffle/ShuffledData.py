0e2920eedf061172008412b1fe9ad8011817d4d7
Arthur Skobnikov <arthur.skobnikov@gmail.com>
2019-11-11 17:13:12 +0100

    (ex.2|shuffle): first working version

---
+++ shuffle_unshuffle/ShuffledData.py
@@ -0,0 +1,76 @@
+import csv
+import random
+import sys
+
+class ShuffledData:
+
+    def __read_csv(self, file_path):
+        table = []
+        with open(file_path, 'r') as file:
+            reader = csv.reader(file)
+            for line in reader:
+                table.append(line)
+        return table
+
+    def __init__(self, file_path):
+        self.table = self.__read_csv(file_path)
+        self.__horizon_shuffle_lst = []
+        self.__vertical_shuffle_lst = []
+
+    def __transpose(self, lst):
+        return list(map(list, zip(*lst)))
+
+    # shuffle
+    def __rotate(self, lst, n):
+        if n > 0:
+            n = n % len(lst)
+        else:
+            n = -(-n % len(lst))
+        return lst[n:] + lst[:n]
+
+    def __shuffle_1d(self, table):
+        width = len(table[0])
+        shuffle_lst = []
+        for i, line in enumerate(table):
+            rand_move = random.randint(-width * 10, width * 10)
+            shuffle_lst.append(rand_move)
+            table[i] = self.__rotate(line, rand_move)
+
+        return shuffle_lst, table
+
+    def shuffle(self):
+        self.__horizon_shuffle_lst, self.table = self.__shuffle_1d(self.table)
+        self.table = self.__transpose(self.table)
+        self.__vertical_shuffle_lst, self.table = self.__shuffle_1d(self.table)
+        self.table = self.__transpose(self.table)
+
+    # unshuffle
+    def __rotate_back(self, lst, n):
+        if n > 0:
+            n = -(n % len(lst))
+        else:
+            n = -n % len(lst)
+        return lst[n:] + lst[:n]
+
+    def __unshuffle_1d(self, shuffle_lst, table):
+        for i, rand_move in enumerate(shuffle_lst):
+            table[i] = self.__rotate_back(table[i], rand_move)
+        return table
+
+    def unshuffle(self):
+        self.table = self.__transpose(self.table)
+        self.table = self.__unshuffle_1d(self.__vertical_shuffle_lst, self.table)
+        self.table = self.__transpose(self.table)
+        self.table = self.__unshuffle_1d(self.__horizon_shuffle_lst, self.table)
+
+
+if __name__ == '__main__':
+
+    file_path = sys.argv[1]
+
+    file = ShuffledData(file_path)
+    print(file.table)
+    file.shuffle()
+    print(file.table)
+    file.unshuffle()
+    print(file.table)
